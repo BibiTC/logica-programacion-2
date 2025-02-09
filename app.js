d5d9e65d32c83b1618802d60f31216ddd5636238
BibiTC <bibiana.trujillo.bt@gmail.com>
2025-02-09 13:31:18 -0500

    Actualiza Actividades Aula 1

---
+++ app.js
@@ -0,0 +1,52 @@
+//==========================================================================================================
+// CODIGO INICIAL DEL AULA1
+// Manejo de HTML y Java
+//-----------------------------------------------------------------------------------------------------------
+
+//Variables
+let titulo = document.querySelector('h1'); // Objeto
+titulo.innerHTML = 'Juego del número secreto';   //poner texto un titulo 
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un número del 1 al 10';
+
+function intentoDeUsuario(){
+
+    alert('click desde el botón');
+}
+
+//==========================================================================================================
+
+
+
+/*
+//==========================================================================================================
+// PREGUNTAS SOBRE EL CODIGO
+//==========================================================================================================
+
+// PREGUNTAS 1: ¿cuál es la forma correcta de crear esta funcionalidad, utilizándose de las buenas prácticas de escritura de las funciones?
+
+<button onclick="alertFunction()">Haz clic aquí</button>
+
+function alertFunction() {
+  alert("¡Hola, has hecho clic en el botón!");
+}
+
+/*
+//==========================================================================================================
+//Lógica de programación: explorar funciones y listas `
+//01. Interactuando con HTML
+//DESAFIO: Hora de Practicar
+//==========================================================================================================
+*/
+
+/*
+//==========================================================================================================
+//ACTIVIDAD 1: 
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+*/
